6f6fe44f8d6f68216c180bb9e26fdae4fc9d7b51
Shivkumar54 <mshivkumar59@gmail.com>
2023-02-06 21:34:13 +0530

    added nav active color

--- src/Layouts/Navbar/Navbar.js
+++ src/Layouts/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 import { HiHome } from "react-icons/hi"
 import { FaUserGraduate } from "react-icons/fa"
 import { MdWork } from "react-icons/md"
@@ -14,39 +14,45 @@ const Navbar = () => {
       setColor(false)
     }
   }
-
+  const location = useLocation()
+  const { pathname } = location
+  const splitLocation = pathname.split("/")
   window.addEventListener("scroll", addBgColor)
   return (
     <div className="entirefile">
       <div className={color ? "navbarRoot header-bg" : "navbarRoot"}>
         <div className="logo">
-          <h1 className="logoName">
-            Portfolio
-          </h1>
+          <h1 className="logoName">Portfolio</h1>
         </div>
         <div className="list">
           <ul className="listUl">
-            <li>
+            <li className={splitLocation[1] === "" ? "active" : "linker"}>
               <Link className="linker" to="/">
                 <HiHome className="iconer" size={20} />
                 Home
               </Link>
             </li>
-            <li>
+            <li className={splitLocation[1] === "about" ? "active" : "linker"}>
               {" "}
               <Link className="linker" to="/about">
                 <FaUserGraduate className="iconer" size={20} />
                 About
               </Link>
             </li>
-            <li>
+            <li
+              className={
+                splitLocation[1] === "projectandskills" ? "active" : "linker"
+              }
+            >
               {" "}
               <Link className="linker" to="/projectandskills">
                 <MdWork className="iconer" size={20} />
                 Projects{" "}
               </Link>
             </li>
-            <li>
+            <li
+              className={splitLocation[1] === "feedback" ? "active" : "linker"}
+            >
               {" "}
               <Link className="linker" to="/feedback">
                 <MdFeedback className="iconer" size={20} />
